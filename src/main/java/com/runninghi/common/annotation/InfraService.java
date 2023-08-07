a549f8f25e5910021d7a43cbfc740f88dec7eae5
manje cho <whakswp123@naver.com>
2023-08-07 17:11:46 +0900

    feat: 초기 파일 세팅

---
+++ src/main/java/com/runninghi/common/annotation/InfraService.java
@@ -0,0 +1,12 @@
+package com.runninghi.common.annotation;
+
+import org.springframework.stereotype.Service;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Service
+public @interface InfraService {
+}
