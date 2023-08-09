5fb8351614ac9897745be2a70f48c8f9036c0f5f
manje cho <whakswp123@naver.com>
2023-08-10 03:36:32 +0900

    (#33)feat: 어노테이션 import 수정

--- src/main/java/com/runninghi/common/entity/BaseTimeEntity.java
+++ src/main/java/com/runninghi/common/entity/BaseTimeEntity.java
@@ -1,11 +1,11 @@
 package com.runninghi.common.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
