b23be59bb75643a16dce8051b7d8b99fbd8c39b8
sui <numerical43@daum.net>
2023-08-11 00:32:28 +0900

    Revert "feature : (#38) 피드백 저장 기능 (entity builder 적용)"
    This reverts commit ffb6ba21476ad5797317faaf7f67a82497773e0d.

--- src/test/java/com/runninghi/feedback/command/service/FeedbackServiceImplTests.java
+++ src/test/java/com/runninghi/feedback/command/service/FeedbackServiceImplTests.java
@@ -1,10 +1,7 @@
 package com.runninghi.feedback.command.service;
 
-import com.runninghi.feedback.command.application.dto.SaveFeedbackDTO;
 import com.runninghi.feedback.command.application.service.FeedbackServiceImpl;
-import com.runninghi.feedback.command.domain.exception.customException.IllegalArgumentException;
 import com.runninghi.feedback.command.domain.repository.FeedbackRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,57 +19,9 @@ public class FeedbackServiceImplTests {
     @Autowired
     private FeedbackRepository feedbackRepository;
 
-    @Test
-    @DisplayName("피드백 저장 테스트 : success")
-    void saveFeedbackTest() {
-        long before = feedbackRepository.count();
-        SaveFeedbackDTO saveFeedbackDTO = new SaveFeedbackDTO("제목", "내용", 0);
-
-        feedbackServiceImpl.saveFeedback(saveFeedbackDTO, 0L);
-
-        long after = feedbackRepository.count();
-
-        Assertions.assertEquals(before + 1, after);
-    }
-
-    @Test
-    @DisplayName("피드백 저장 테스트 : 제목 500자 제한 확인")
-    void checkFeedbackTitleTest() {
-        String str = "a".repeat(501);
-
-        long before = feedbackRepository.count();
-        SaveFeedbackDTO saveFeedbackDTO = new SaveFeedbackDTO(str, "내용", 0);
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> feedbackServiceImpl.saveFeedback(saveFeedbackDTO, 0L));
-
-        long after = feedbackRepository.count();
-        Assertions.assertEquals(before, after);
-    }
-
-    @Test
-    @DisplayName("피드백 저장 테스트 : 내용 null 제한 확인")
-    void checkFeedbackContentTest() {
-        String str = "";
-
-        long before = feedbackRepository.count();
-        SaveFeedbackDTO saveFeedbackDTO = new SaveFeedbackDTO("제목", str, 0);
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> feedbackServiceImpl.saveFeedback(saveFeedbackDTO, 0L));
-
-        long after = feedbackRepository.count();
-        Assertions.assertEquals(before, after);
-    }
-
-    @Test
-    @DisplayName("피드백 저장 테스트 : 카테고리 번호가 올바른지 확인")
-    void checkFeedbackCategoryTest() {
-        long before = feedbackRepository.count();
-        SaveFeedbackDTO saveFeedbackDTO = new SaveFeedbackDTO("제목", "내용", 1000);
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> feedbackServiceImpl.saveFeedback(saveFeedbackDTO, 0L));
-
-        long after = feedbackRepository.count();
-        Assertions.assertEquals(before, after);
-    }
-
+//    @Test
+//    @DisplayName("피드백 저장 테스트")
+//    void saveFeedbackTest() {
+//
+//    }
 }
