9b9760e11c67f0eabd2d36b824c85834c2560855
manje cho <whakswp123@naver.com>
2023-08-10 10:15:03 +0900

    [#4, #6, #33]feat: 유저 서비스 수정

--- src/main/java/com/runninghi/user/command/domain/repository/UserRepository.java
+++ src/main/java/com/runninghi/user/command/domain/repository/UserRepository.java
@@ -1,11 +1,19 @@
 package com.runninghi.user.command.domain.repository;
 
 import com.runninghi.user.command.domain.aggregate.entity.User;
+import com.runninghi.user.command.domain.aggregate.entity.enumtype.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 @Transactional
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByAccount(String account);
+
+    List<User> findAllByType(Role role);
 }
