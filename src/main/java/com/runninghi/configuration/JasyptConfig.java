884d8275aaef7bdf76fdf2b53e7d32204a0c2cd6
numerical43 <numerical43@daum.net>
2023-08-11 16:59:01 +0900

    setting : (#44) Jasypt 이용하여 민감정보 암호화

---
+++ src/main/java/com/runninghi/configuration/JasyptConfig.java
@@ -0,0 +1,36 @@
+package com.runninghi.configuration;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "com.runninghi")
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Value("${APP_EN}")
+    private  String appEn;
+
+    @Bean("jasyptStringEncryptor")
+    @Primary
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setPoolSize(5);
+        config.setPassword(appEn);
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
