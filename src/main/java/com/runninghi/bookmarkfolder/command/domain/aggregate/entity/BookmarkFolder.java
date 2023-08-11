821fb453c349d60addeae1d3587b788ce2de482c
manje cho <whakswp123@naver.com>
2023-08-11 15:27:57 +0900

    feat: import 수정

--- src/main/java/com/runninghi/bookmarkfolder/command/domain/aggregate/entity/BookmarkFolder.java
+++ src/main/java/com/runninghi/bookmarkfolder/command/domain/aggregate/entity/BookmarkFolder.java
@@ -3,9 +3,9 @@ package com.runninghi.bookmarkfolder.command.domain.aggregate.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 
 
 @Entity
