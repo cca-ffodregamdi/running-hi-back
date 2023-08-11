9d2946408408ab0128e91eff9a09f9c86339c3d2
manje cho <whakswp123@naver.com>
2023-08-11 15:28:15 +0900

    feat: import 수정

--- src/main/java/com/runninghi/commentReport/command/domain/aggregate/vo/ReportedCommentVO.java
+++ src/main/java/com/runninghi/commentReport/command/domain/aggregate/vo/ReportedCommentVO.java
@@ -1,11 +1,11 @@
 package com.runninghi.commentreport.command.domain.aggregate.vo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
