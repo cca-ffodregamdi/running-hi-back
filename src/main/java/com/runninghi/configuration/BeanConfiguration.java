e28722514d77db8f12b1e2112c1c35b60ce6785f
Son Jung In <jungin.dylan@gmail.com>
2023-08-11 19:54:26 +0900

    bugfix: jasypt 오류 해결

---
+++ src/main/java/com/runninghi/configuration/BeanConfiguration.java
@@ -0,0 +1,10 @@
+package com.runninghi.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.runninghi")
+public class BeanConfiguration {
+
+}
