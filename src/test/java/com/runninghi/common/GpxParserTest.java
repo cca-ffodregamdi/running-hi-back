9d7f3d210c9f1ec1087328d7404c278e79f45b05
UncleSubak <kko6369@naver.com>
2023-08-15 18:22:09 +0900

    Test: 테스트 파일 생성과 테스트 코드 작성

--- src/test/java/com/runninghi/common/GpxParserTest.java
+++ src/test/java/com/runninghi/common/GpxParserTest.java
@@ -1,21 +1,17 @@
 package com.runninghi.common;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 
-@SpringBootTest
 class GpxParserTest {
 
     GpxParser gpxParser = new GpxParser();
 
-    @DisplayName("gpx parser 테스트")
+    @DisplayName("gpx parser 테스트 : success")
     @Test
     void testGPXparser() throws ParserConfigurationException {
 
@@ -26,6 +22,20 @@ class GpxParserTest {
         Assertions.assertDoesNotThrow(
                 () -> gpxParser.parseGPXFile(gpxFile)
         );
+    }
+
+    @DisplayName("gpx parser 테스트 : 파일 내 기록에 이상한 값이 들어가있을 때도 저장이 되는지")
+    @Test
+    void testWrongGPXparser() {
+
+        // given
+        File worngGpxFile = new File("src/main/resources/gpxtest/wrongTest.gpx"); // 기록 중간에 글자가 있고 빈 칸이 있음.
+
+        // when & then
+        Assertions.assertDoesNotThrow(
+                () -> gpxParser.parseGPXFile(worngGpxFile)
+        );
+
     }
 
 }
