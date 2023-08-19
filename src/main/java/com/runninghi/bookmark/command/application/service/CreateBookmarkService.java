a1dd57ea1a256eeeb83218ff86feefe960e55d11
Son Jung In <88484476+Dylan-SonJungin@users.noreply.github.com>
2023-08-20 00:19:49 +0900

    fix: interface 수정

--- src/main/java/com/runninghi/bookmark/command/application/service/CreateBookmarkService.java
+++ src/main/java/com/runninghi/bookmark/command/application/service/CreateBookmarkService.java
@@ -3,6 +3,7 @@ package com.runninghi.bookmark.command.application.service;
 import com.runninghi.bookmark.command.application.dto.request.CreateBookmarkRequest;
 import com.runninghi.bookmark.command.domain.aggregate.entity.Bookmark;
 import com.runninghi.bookmark.command.domain.repository.BookmarkRepository;
+import com.runninghi.bookmark.command.domain.service.BookmarkDomainService;
 import com.runninghi.bookmark.command.infrastructure.service.BookmarkInfraService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +19,13 @@ public class CreateBookmarkService {
     @Autowired
     BookmarkRepository bookmarkRepository;
     @Autowired
-    BookmarkInfraService infraService;
+    BookmarkDomainService bookmarkDomainService;
 
     @Transactional
     public Bookmark createBookmark(CreateBookmarkRequest bookmarkDTO) {
 
         //infraService.validatePostExist(bookmarkDTO.bookmarkVO().getPostNo());
-        infraService.validateFolderExist(bookmarkDTO.bookmarkVO().getFolderNo());
+        bookmarkDomainService.validateFolderExist(bookmarkDTO.bookmarkVO().getFolderNo());
 
         return bookmarkRepository.save(Bookmark.builder()
                 .bookmarkVO(bookmarkDTO.bookmarkVO())
