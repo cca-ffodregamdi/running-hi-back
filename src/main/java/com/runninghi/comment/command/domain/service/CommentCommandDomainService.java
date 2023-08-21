7d68c31c56495a04172540e318f001e814aa69f3
Son Jung In <88484476+Dylan-SonJungin@users.noreply.github.com>
2023-08-22 08:42:08 +0900

    feat: #136 #137

--- src/main/java/com/runninghi/comment/command/domain/service/CommentCommandDomainService.java
+++ src/main/java/com/runninghi/comment/command/domain/service/CommentCommandDomainService.java
@@ -4,9 +4,9 @@ import com.runninghi.common.annotation.DomainService;
 
 import java.util.UUID;
 
-@DomainService
 public interface CommentCommandDomainService {
     void validateCommentContentNull(String commentContent);
     void validateUser(UUID userNo);
     void validateUserPost(Long userPostNo);
+    void validateComment(Long commentNo);
 }
