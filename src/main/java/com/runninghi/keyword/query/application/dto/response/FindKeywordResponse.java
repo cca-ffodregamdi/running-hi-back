d26525cd2006a3f3a10e739389377b2114cfbb82
UncleSubak <kko6369@naver.com>
2023-08-21 19:44:31 +0900

    Feat : 키워드 조회 기능 구현

---
+++ src/main/java/com/runninghi/keyword/query/application/dto/response/FindKeywordResponse.java
@@ -0,0 +1,18 @@
+package com.runninghi.keyword.query.application.dto.response;
+
+import com.runninghi.keyword.command.domain.aggregate.entity.Keyword;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record FindKeywordResponse(
+        @Schema(description = "키워드 번호")
+        Long keywordNo,
+        @Schema(description = "키워드 제목")
+        String keywordName
+) {
+        public static FindKeywordResponse from(Keyword keyword) {
+                return new FindKeywordResponse(
+                        keyword.getKeywordNo(),
+                        keyword.getKeywordName()
+                );
+        }
+}
