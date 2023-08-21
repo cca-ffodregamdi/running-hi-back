0cfa45825af36828382b675aadb21ad876fc6b51
hyojin lee <hodin030@gmail.com>
2023-08-21 18:49:09 +0900

    feat: 게시글신고 조회 기능

--- src/main/java/com/runninghi/postreport/query/application/service/PostReportQueryService.java
+++ src/main/java/com/runninghi/postreport/query/application/service/PostReportQueryService.java
@@ -1,8 +1,9 @@
 package com.runninghi.postreport.query.application.service;
 
+import com.runninghi.feedback.command.domain.exception.customException.NotFoundException;
 import com.runninghi.postreport.command.application.dto.response.PostReportResponse;
 import com.runninghi.postreport.command.domain.aggregate.entity.PostReport;
-import com.runninghi.postreport.command.domain.repository.PostReportRepository;
+import com.runninghi.postreport.command.domain.repository.PostReportCommandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,19 +11,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class FindPostReportService {
+public class PostReportQueryService {
 
-    private PostReportRepository postReportRepository;
+    private PostReportCommandRepository postReportCommandRepository;
 
     // 설명. 게시글 신고 상세 조회
     public PostReportResponse findPostReport(Long postReportNo) {
 
-        // 설명. findById는 Optional<PostReport> 형태로 객체 반환. get()을 통해 Optional 안의 객체를 꺼냄.
-        PostReport postReport = postReportRepository.findById(postReportNo).get();
+        // 설명. findById는 Optional<PostReport> 형태로 객체 반환.
+        PostReport postReport = postReportCommandRepository.findById(postReportNo)
+                .orElseThrow(() -> new NotFoundException("해당하는 신고 내역이 없습니다."));
 
-        if(postReport == null) {
-            throw new IllegalArgumentException("해당하는 신고 내역이 없습니다.");
-        }
 
         // 설명. Entity -> DTO 변환
         PostReportResponse postReportResponse = PostReportResponse.from(postReport);
@@ -33,9 +32,13 @@ public class FindPostReportService {
     // 설명. 모든 게시글 신고 조회
     public List<PostReportResponse> findAllPostReports() {
 
-        List<PostReportResponse> postReportList = postReportRepository.findAll().stream()
+        List<PostReportResponse> postReportList = postReportCommandRepository.findAll().stream()
                 .map(PostReportResponse::from)
                 .toList();
+
+        if(postReportList.size() == 0) {
+            throw new NotFoundException("신고 내역이 없습니다.");
+        }
 
         return postReportList;
     }
