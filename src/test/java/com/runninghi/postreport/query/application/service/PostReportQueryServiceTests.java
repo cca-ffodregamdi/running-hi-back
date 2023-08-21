7d27defd7e0dd2f205a5fb43e98733860927b2f6
hyojin lee <hodin030@gmail.com>
2023-08-21 20:08:25 +0900

    test: 게시글 신고 조회 테스트코드

---
+++ src/test/java/com/runninghi/postreport/query/application/service/PostReportQueryServiceTests.java
@@ -0,0 +1,86 @@
+package com.runninghi.postreport.query.application.service;
+
+import com.runninghi.feedback.command.domain.exception.customException.NotFoundException;
+import com.runninghi.postreport.command.application.dto.request.PostReportRequest;
+import com.runninghi.postreport.command.application.dto.response.PostReportResponse;
+import com.runninghi.postreport.command.application.service.PostReportCommandService;
+import com.runninghi.postreport.command.domain.aggregate.entity.PostReport;
+import com.runninghi.postreport.command.domain.aggregate.vo.PostReportUserVO;
+import com.runninghi.postreport.command.domain.aggregate.vo.PostReportedUserVO;
+import com.runninghi.postreport.command.domain.aggregate.vo.ReportedPostVO;
+import com.runninghi.postreport.command.domain.repository.PostReportCommandRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Transactional
+public class PostReportQueryServiceTests {
+
+    @Autowired
+    private PostReportCommandRepository postReportCommandRepository;
+
+    @Autowired
+    private PostReportCommandService postReportCommandService;
+
+    @Autowired
+    private PostReportQueryService postReportQueryService;
+
+    @Test
+    @DisplayName("게시글 신고 조회 테스트: 상세조회 성공")
+    void findPostReportTest() {
+
+        //given
+        PostReportRequest postReportRequest = new PostReportRequest(2, "홍보 게시물");
+
+        //when
+        PostReport savedPostReport = postReportCommandService.savePostReport(postReportRequest);
+
+        PostReport findedPostReport = postReportCommandRepository.findById(savedPostReport.getPostReportNo()).get();
+
+        //then
+        assertThat(findedPostReport.getPostReportContent().equals("홍보 게시물"));
+        assertThat(findedPostReport.getPostReportCategoryCode()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("게시글 신고 조회 테스트: 전체조회 성공")
+    void findPostReportListTest() {
+
+        //given
+        postReportCommandService.savePostReport(new PostReportRequest(1, "욕설"));
+        postReportCommandService.savePostReport(new PostReportRequest(2, "홍보"));
+        postReportCommandService.savePostReport(new PostReportRequest(3, "도배"));
+
+        //when
+        List<PostReport> postReportList = postReportCommandRepository.findAll();
+
+        //then
+        Assertions.assertEquals(3, postReportList.size());
+    }
+
+//    @Test
+//    @DisplayName("게시글 신고 조회 테스트: 조회할 게시글 신고 번호 없을 시 예외처리 확인")
+//    void doesNotExistPostReportTest() {
+//
+//        //given
+//        postReportCommandService.savePostReport(new PostReportRequest(1, "욕설"));
+//
+//        //when & then
+////        assertThatThrownBy(() -> postReportQueryService.findPostReport(10L))
+////                .isInstanceOf(NotFoundException.class)
+////                .hasMessage("해당하는 신고 내역이 없습니다.");
+//
+//        Assertions.assertThrows(NotFoundException.class, () -> postReportQueryService.findPostReport(10L));
+//    }
+}
