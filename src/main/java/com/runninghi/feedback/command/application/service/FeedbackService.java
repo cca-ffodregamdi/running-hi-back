14e8614f3fde32f7bc3567c951ad8be32f12f996
manje cho <whakswp123@naver.com>
2023-08-22 09:22:48 +0900

    [#134]refactoring: 클래스명 및 테스트 코드 수정

--- src/main/java/com/runninghi/feedback/command/application/service/FeedbackService.java
+++ src/main/java/com/runninghi/feedback/command/application/service/FeedbackService.java
@@ -8,36 +8,39 @@ import com.runninghi.feedback.command.application.dto.request.FeedbackCreateRequ
 import com.runninghi.feedback.command.application.dto.request.FeedbackDeleteRequest;
 import com.runninghi.feedback.command.application.dto.request.FeedbackUpdateRequest;
 import com.runninghi.feedback.command.application.dto.response.FeedbackResponse;
-import com.runninghi.feedback.command.application.dto.response.FeedbackUserResponse;
 import com.runninghi.feedback.command.domain.aggregate.entity.Feedback;
 import com.runninghi.feedback.command.domain.aggregate.entity.FeedbackCategory;
 import com.runninghi.feedback.command.domain.aggregate.vo.FeedbackWriterVO;
 import com.runninghi.feedback.command.domain.repository.FeedbackRepository;
-import com.runninghi.feedback.command.domain.service.FeedbackCommandDomainService;
+import com.runninghi.user.command.domain.aggregate.entity.User;
+import com.runninghi.user.command.domain.aggregate.entity.enumtype.Role;
+import com.runninghi.user.command.domain.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.UUID;
 
 
 @Service
 @RequiredArgsConstructor
 public class FeedbackService {
-
     private final FeedbackRepository feedbackRepository;
-
-    private final FeedbackCommandDomainService feedbackCommandDomainService;
+    private final UserRepository userRepository;
 
     // 피드백 저장
     @Transactional
-    public FeedbackResponse createFeedback(FeedbackCreateRequest feedbackCreateRequest, FeedbackUserResponse user) {
+    public FeedbackResponse createFeedback(FeedbackCreateRequest feedbackCreateRequest, UUID userId) {
+
+        User writer = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("존재하지않는 회원입니다."));
 
         // 피드백 제한사항 확인(제목, 내용 길이)
         checkFeedbackValidation(feedbackCreateRequest.feedbackTitle(), feedbackCreateRequest.feedbackContent());
 
         // 작성자 vo 생성
-        FeedbackWriterVO feedbackWriterVO = new FeedbackWriterVO(user.id());
+        FeedbackWriterVO feedbackWriterVO = new FeedbackWriterVO(userId);
 
         Feedback feedback = new Feedback.Builder()
                 .feedbackTitle(feedbackCreateRequest.feedbackTitle())
@@ -55,14 +58,16 @@ public class FeedbackService {
 
     // 피드백 수정
     @Transactional
-    public FeedbackResponse updateFeedback(FeedbackUpdateRequest feedbackUpdateRequest, FeedbackUserResponse user) {
+    public FeedbackResponse updateFeedback(FeedbackUpdateRequest feedbackUpdateRequest, UUID userId) {
 
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("존재하지않는 회원입니다."));
 
         Feedback feedback = feedbackRepository.findByFeedbackNo(feedbackUpdateRequest.feedbackNo())
                 .orElseThrow(() -> new NotFoundException("존재하지않는 피드백입니다."));
 
         // 작성자와 현재 회원이 일치하는지 확인
-        if (!feedbackCommandDomainService.isWriter(user, feedback))
+        if (!isWriter(userId, feedback))
             throw new NotMatchWriterException("작성자와 회원이 일치하지않습니다.");
 
         // 피드백 제한사항 확인(제목, 내용 길이)
@@ -90,13 +95,16 @@ public class FeedbackService {
 
     // 피드백 삭제
     @Transactional
-    public void deleteFeedback(FeedbackDeleteRequest feedbackDeleteRequest, FeedbackUserResponse user) {
+    public void deleteFeedback(FeedbackDeleteRequest feedbackDeleteRequest, UUID userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("존재하지않는 회원입니다."));
 
         Feedback feedback = feedbackRepository.findByFeedbackNo(feedbackDeleteRequest.feedbackNo())
                 .orElseThrow(() -> new NotFoundException("존재하지않는 피드백입니다."));
 
         // 작성자와 현재 회원이 일치하는지 확인
-        if (!feedbackCommandDomainService.isWriter(user, feedback))
+        if (!isWriter(userId, feedback))
             throw new NotMatchWriterException("작성자와 회원이 일치하지않습니다.");
 
         feedbackRepository.delete(feedback);
@@ -105,13 +113,15 @@ public class FeedbackService {
 
     // 관리자가 피드백 삭제
     @Transactional
-    public void deleteFeedbackByAdmin(FeedbackDeleteRequest feedbackDeleteRequest, FeedbackUserResponse user) {
+    public void deleteFeedbackByAdmin(FeedbackDeleteRequest feedbackDeleteRequest, UUID userId) {
 
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("존재하지않는 회원입니다."));
         Feedback feedback = feedbackRepository.findByFeedbackNo(feedbackDeleteRequest.feedbackNo())
                 .orElseThrow(() -> new NotFoundException("존재하지않는 피드백입니다."));
 
         // 작성자와 현재 회원이 일치하는지 확인
-        if (!feedbackCommandDomainService.isAdminRole(user))
+        if (!isAdminRole(user))
             throw new UnauthorizedAccessException("관리자가 아닙니다.");
 
         feedbackRepository.delete(feedback);
@@ -132,6 +142,20 @@ public class FeedbackService {
         if (content.length() == 0) {
             throw new IllegalArgumentException("내용은 1글자 이상이어야 합니다.");
         }
+
+    }
+
+    // 피드백 수정/삭제 요청자와 피드백 작성자가 동일한지 확인
+    private boolean isWriter(UUID userId, Feedback feedback) {
+
+        return userId.equals(feedback.getFeedbackWriterVO().getFeedbackWriterId());
+
+    }
+
+    // 요청자가 관리자인지 확인
+    private boolean isAdminRole(User user) {
+
+        return user.getRole().equals(Role.ADMIN);
 
     }
 
