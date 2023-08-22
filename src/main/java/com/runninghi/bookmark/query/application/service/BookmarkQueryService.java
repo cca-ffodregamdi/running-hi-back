6e116f634614a9a6596bfa8176d45bc956716ad3
Son Jung In <88484476+Dylan-SonJungin@users.noreply.github.com>
2023-08-22 10:00:34 +0900

    feat: 전체 즐겨찾기 조회 추가

--- src/main/java/com/runninghi/bookmark/query/application/service/BookmarkQueryService.java
+++ src/main/java/com/runninghi/bookmark/query/application/service/BookmarkQueryService.java
@@ -2,6 +2,7 @@ package com.runninghi.bookmark.query.application.service;
 
 import com.runninghi.bookmark.command.domain.aggregate.entity.Bookmark;
 import com.runninghi.bookmark.command.domain.repository.BookmarkRepository;
+import com.runninghi.bookmark.command.domain.service.BookmarkCommandDomainService;
 import com.runninghi.bookmark.query.application.dto.FindBookmarkListRequest;
 import com.runninghi.bookmark.query.application.dto.FindBookmarkRequest;
 import com.runninghi.common.handler.feedback.customException.NotFoundException;
@@ -16,6 +17,7 @@ import java.util.List;
 public class BookmarkQueryService {
 
     private final BookmarkRepository bookmarkRepository;
+    private final BookmarkCommandDomainService bookmarkDomainService;
     @Transactional(readOnly=true)
     public Bookmark findBookmark(FindBookmarkRequest bookmarkDTO) {
         return bookmarkRepository.findById(bookmarkDTO.bookmarkVO())
@@ -24,7 +26,7 @@ public class BookmarkQueryService {
 
     @Transactional(readOnly = true)
     public List<Bookmark> findBookmarkListByFolder(FindBookmarkListRequest bookmarkDTO) {
-
+        bookmarkDomainService.validateFolderExist(bookmarkDTO.folderNo());
         return bookmarkRepository.findBookmarkByBookmarkVO_FolderNo(bookmarkDTO.folderNo());
     }
 }
