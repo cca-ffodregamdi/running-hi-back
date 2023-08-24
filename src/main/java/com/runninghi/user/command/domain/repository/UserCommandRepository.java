3bf95d065ffc7ebaa551e9816832139cfe11baf6
manje cho <whakswp123@naver.com>
2023-08-24 17:52:21 +0900

    [#121]refactoring: 레포지토리 커맨드 쿼리로 분리

--- src/main/java/com/runninghi/user/command/domain/repository/UserCommandRepository.java
+++ src/main/java/com/runninghi/user/command/domain/repository/UserCommandRepository.java
@@ -1,20 +1,17 @@
 package com.runninghi.user.command.domain.repository;
 
 import com.runninghi.user.command.domain.aggregate.entity.User;
-import com.runninghi.user.command.domain.aggregate.entity.enumtype.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 @Transactional
 public interface UserCommandRepository extends JpaRepository<User, UUID> {
-    Optional<User> findUserByAccount(String account);
-
-    List<User> findAllUserByRole(Role role);
+//    Optional<User> findUserByAccount(String account);
+//
+//    List<User> findAllUserByRole(Role role);
 }
 
