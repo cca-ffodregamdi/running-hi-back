eb51c050155865e95a339cfd644b2a416ce21a5d
Son Jung In <88484476+Dylan-SonJungin@users.noreply.github.com>
2023-08-25 13:35:08 +0900

    feat: #143

--- src/main/java/com/runninghi/bookmark/command/domain/service/BookmarkCommandDomainService.java
+++ src/main/java/com/runninghi/bookmark/command/domain/service/BookmarkCommandDomainService.java
@@ -1,9 +1,10 @@
 package com.runninghi.bookmark.command.domain.service;
 
+import com.runninghi.bookmark.command.domain.aggregate.vo.BookmarkVO;
 import com.runninghi.common.annotation.DomainService;
 
-@DomainService
 public interface BookmarkCommandDomainService {
     void validatePostExist(Long postNo);
     void validateFolderExist(Long folderNo);
+    void validateBookmarkExist(BookmarkVO bookmarkVO);
 }
