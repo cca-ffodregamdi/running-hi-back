ec08df1628db504f7146f17a67e29f428f8f27ba
hyojin lee <hodin030@gmail.com>
2023-08-27 21:07:32 +0900

    feat: 피신고자 정보 가져오기

--- src/main/java/com/runninghi/postreport/command/infrastructure/service/PostReportCommandInfraService.java
+++ src/main/java/com/runninghi/postreport/command/infrastructure/service/PostReportCommandInfraService.java
@@ -1,7 +1,25 @@
 package com.runninghi.postreport.command.infrastructure.service;
 
 import com.runninghi.common.annotation.InfraService;
+import com.runninghi.postreport.command.application.dto.response.ReportedUserResponse;
+import com.runninghi.postreport.command.domain.service.PostReportCommandDomainService;
+import com.runninghi.user.command.application.dto.user.response.UserInfoResponse;
+import com.runninghi.user.command.application.service.UserService;
+import lombok.RequiredArgsConstructor;
+import java.util.UUID;
 
 @InfraService
-public class PostReportCommandInfraService {
+@RequiredArgsConstructor
+public class PostReportCommandInfraService implements PostReportCommandDomainService {
+
+    private final UserService userService;
+
+    public ReportedUserResponse getReportedUserInfo(UUID reportedUserNo) {
+
+        UserInfoResponse userinfo = userService.findUserInfo(reportedUserNo);
+
+        ReportedUserResponse reportedUserResponse = new ReportedUserResponse(userinfo.reportCount(), userinfo.blackListStatus());
+
+        return reportedUserResponse;
+    }
 }
