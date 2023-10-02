9578cc462f6e481900df4a33a1f11a8f3fe96e1f
manje cho <whakswp123@naver.com>
2023-10-03 00:58:41 +0900

    [#201]feat: log4j2 설정

--- src/main/java/com/runninghi/common/exception/ExceptionResponseHandler.java
+++ src/main/java/com/runninghi/common/exception/ExceptionResponseHandler.java
@@ -5,7 +5,6 @@ import com.runninghi.common.response.ApiResponse;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.SignatureException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -14,18 +13,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
-@Slf4j
+
 @RestControllerAdvice
 public class ExceptionResponseHandler {
     @ExceptionHandler({IllegalArgumentException.class, NoSuchElementException.class})
     public ResponseEntity<ApiResponse> handleCommonException(Exception e) {
-        log.error(e.getMessage());
         return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
     }
 
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<ApiResponse> handleNullPointerException(NullPointerException e) {
-        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(ApiResponse.error(e.getMessage()));
     }
 
@@ -56,7 +53,6 @@ public class ExceptionResponseHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiResponse> handleNotFoundException(NotFoundException e) {
-        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.error(e.getMessage()));
     }
 }
