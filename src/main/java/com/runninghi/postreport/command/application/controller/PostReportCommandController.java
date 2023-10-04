e788c7b29bc0f13aa916f3ea8209e26b2650c638
hodin <hodin030@gmail.com>
2023-10-04 17:57:25 +0900

    feat: 게시글 신고 CRUD 컨트롤러 작성

--- src/main/java/com/runninghi/postreport/command/application/controller/PostReportCommandController.java
+++ src/main/java/com/runninghi/postreport/command/application/controller/PostReportCommandController.java
@@ -1,11 +1,42 @@
 package com.runninghi.postreport.command.application.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.runninghi.common.response.ApiResponse;
+import com.runninghi.postreport.command.application.dto.request.PostReportSaveRequest;
+import com.runninghi.postreport.command.application.dto.request.PostReportUpdateRequest;
+import com.runninghi.postreport.command.application.dto.response.PostReportDeleteResponse;
+import com.runninghi.postreport.command.application.dto.response.PostReportResponse;
+import com.runninghi.postreport.command.application.service.PostReportCommandService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/postreport")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class PostReportCommandController {
 
+    private final PostReportCommandService postReportCommandService;
 
+    @PostMapping("/postReports")
+    public ApiResponse createPostReport(PostReportSaveRequest request) {
+
+        PostReportResponse response = postReportCommandService.savePostReport(request);
+
+        return ApiResponse.success("성공적으로 등록되었습니다.", response);
+    }
+
+    @PutMapping("/postReports/{postReportNo}")
+    public ApiResponse updatePostReport(PostReportUpdateRequest request, @PathVariable Long postReportNo) {
+
+        PostReportResponse response = postReportCommandService.updatePostReport(request, postReportNo);
+
+        return ApiResponse.success("성공적으로 처리되었습니다.", response);
+    }
+
+    @DeleteMapping("/postReports/{postReportNo}")
+    public ApiResponse deletePostReport(@PathVariable Long postReportNo) {
+
+        PostReportDeleteResponse response = postReportCommandService.deletePostReport(postReportNo);
+
+        return ApiResponse.success("성공적으로 삭제되었습니다.", response);
+    }
 }
