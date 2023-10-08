14bb791e926b5fa9d60645e9a93e36f036189fef
sui <numerical43@daum.net>
2023-10-08 16:40:56 +0900

    feature : ImageCommandService의 s3 부분 삭제, storage의 이미지 삭제하는 기능과 이미지 entity 삭제하는 기능 분리

--- src/main/java/com/runninghi/image/command/domain/repository/ImageCommandRepository.java
+++ src/main/java/com/runninghi/image/command/domain/repository/ImageCommandRepository.java
@@ -1,24 +1,25 @@
 package com.runninghi.image.command.domain.repository;
 
 import com.runninghi.image.command.domain.aggregate.entity.Image;
-import com.runninghi.image.command.domain.aggregate.vo.AdminPostVO;
-import com.runninghi.image.command.domain.aggregate.vo.UserPostVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ImageCommandRepository extends JpaRepository<Image, Long> {
 
     // 특정 유저 게시물과 관련된 이미지들 전부 삭제
-    void deleteImagesByUserPostVO(UserPostVO userPostVO);
+    void deleteImagesByUserPostVO_UserPostNo(Long userPostNo);
 
-    @Query(value = "SELECT i.imageUrl FROM Image i WHERE i.userPostVO = :?1", nativeQuery = true)
-    List<String> getImagesByUserPostVO(UserPostVO userPostVO);
+    // 특정 유저 게시물과 관련된 이미지 url list 조회
+    @Query(value = "SELECT i.image_url FROM tbl_image i WHERE i.user_post_no = :userPostNo", nativeQuery = true)
+    List<String> getImagesByUserPostNo(@Param("userPostNo") Long userPostNo);
 
     // 특정 관리자 게시물과 관련된 이미지들 전부 삭제
-    void deleteImagesByAdminPostVO(AdminPostVO adminPostVO);
+    void deleteImagesByAdminPostVO_AdminPostNo(Long adminPostNo);
 
-    @Query(value = "SELECT i.imageUrl FROM Image i WHERE i.adminPostVO = :?1", nativeQuery = true)
-    List<String> getImagesByAdminPostVO(AdminPostVO adminPostVO);
+    // 특정 관리자 게시물과 관련된 이미지 url list 조회
+    @Query(value = "SELECT i.image_url FROM tbl_image i WHERE i.admin_post_no = :adminPostNo", nativeQuery = true)
+    List<String> getImagesByAdminPostNo(@Param("adminPostNo") Long adminPostNo);
 }
