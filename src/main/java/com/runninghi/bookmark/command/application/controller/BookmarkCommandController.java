3db722194c9eba0ac7756e3b18c2f9d7e0ab46b0
SonJungin <jungin.dylan@gmail.com>
2023-11-19 14:25:58 +0900

    feat: #207 즐겨찾기 controller 작성

--- src/main/java/com/runninghi/bookmark/command/application/controller/BookmarkCommandController.java
+++ src/main/java/com/runninghi/bookmark/command/application/controller/BookmarkCommandController.java
@@ -1,14 +1,42 @@
 package com.runninghi.bookmark.command.application.controller;
 
+import com.runninghi.bookmark.command.application.dto.request.CreateBookmarkRequest;
+import com.runninghi.bookmark.command.application.dto.request.DeleteBookmarkRequest;
+import com.runninghi.bookmark.command.application.dto.response.BookmarkCommandResponse;
+import com.runninghi.bookmark.command.application.dto.response.BookmarkDeleteResponse;
+import com.runninghi.bookmark.command.application.service.BookmarkCommandService;
+import com.runninghi.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "즐겨찾기 Command API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/bookmark")
 public class BookmarkCommandController {
+
+
+    private final BookmarkCommandService bookmarkCommandService;
+
+    @Operation(summary = "즐겨찾기 추가")
+    @PostMapping("/api/v1/bookmarks")
+    public ResponseEntity<ApiResponse> createBookmark(CreateBookmarkRequest request) {
+
+        BookmarkCommandResponse response = bookmarkCommandService.createBookmark(request);
+
+        return ResponseEntity.ok(ApiResponse.success("성공적으로 등록되었습니다.", response));
+    }
+
+    @Operation(summary = "즐겨찾기 취소")
+    @DeleteMapping("/api/v1/bookmarks")
+    public ResponseEntity<ApiResponse> deleteBookmark(DeleteBookmarkRequest request) {
+
+        BookmarkDeleteResponse response = bookmarkCommandService.deleteBookmark(request);
+
+        return ResponseEntity.ok(ApiResponse.success("성공적으로 삭제되었습니다.", response));
+    }
 
 }
