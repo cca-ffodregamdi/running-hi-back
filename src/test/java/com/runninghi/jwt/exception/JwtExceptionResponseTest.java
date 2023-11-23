b08441d8ad0b77310eda33929ad1e9147d866932
manje cho <whakswp123@naver.com>
2023-11-24 01:28:09 +0900

    [#235]refactor: 매핑 오타 수정

--- src/test/java/com/runninghi/jwt/exception/JwtExceptionResponseTest.java
+++ src/test/java/com/runninghi/jwt/exception/JwtExceptionResponseTest.java
@@ -31,7 +31,7 @@ public class JwtExceptionResponseTest {
     }
 
     private MockHttpServletRequestBuilder request() {
-        return get("/api/vi/admin/users").contentType(MediaType.APPLICATION_JSON);
+        return get("/api/v1/admin/users").contentType(MediaType.APPLICATION_JSON);
     }
 
     @Test
