6b1aeedc02832bcdc08ce8a8e8c1010eb387021a
manje cho <whakswp123@naver.com>
2023-11-24 01:32:14 +0900

    [#235]refactor: 아이디/비밀번호 찾기 매핑 주소 허용

--- src/main/java/com/runninghi/configuration/SecurityConfig.java
+++ src/main/java/com/runninghi/configuration/SecurityConfig.java
@@ -17,7 +17,8 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
-    private static final String[] ALLOWED_URIS = {"/sign-up", "/sign-in"};
+    private static final String[] ALLOWED_URIS = {"/api/v1/sign-up", "/api/v1/sign-in", "/api/v1/verify-duplication-id"
+            , "/api/v1/member/find-account", "/api/v1/member/find-password"};
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationEntryPoint entryPoint;
 
@@ -25,13 +26,13 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .headers(headers -> headers.frameOptions().sameOrigin())
                 .authorizeHttpRequests(request ->
                         request.requestMatchers(ALLOWED_URIS).permitAll()
                                 .anyRequest().authenticated()
-                )
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
                 .exceptionHandling(handler -> handler.authenticationEntryPoint(entryPoint))
